afa2bd211d6113480cf039b48a66a07057926ff0
Alrykemess <alrykemes.cavalcanti@anota.ai>
2025-10-27 21:34:11 -0300

    feat: function generate population

---
+++ AG_TSP/ag_tsp.py
@@ -0,0 +1,18 @@
+import random
+from .TSP_13_cities.tsp_utils import calculate_distance
+
+def gerar_populacao(tamanho_populacao):
+    # Gera uma população inicial aleatória
+    populacao = []
+    ex_route = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    for _ in range(tamanho_populacao):
+        individuo = random.sample(ex_route, len(ex_route))
+        populacao.append(individuo)
+    return populacao
+
+# === Execução principal ===
+if __name__ == "__main__":
+    populacao = gerar_populacao(tamanho_populacao=50)
+    for individuo in populacao:
+        print(individuo, "\n")
+        print(calculate_distance(individuo), "\n")
